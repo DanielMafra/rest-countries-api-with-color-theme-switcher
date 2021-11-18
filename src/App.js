cb236b681d21b78d39cf6fbe3400ca4cd6c9c92b
Daniel Mafra <danielmafradev@gmail.com>
2021-11-18 10:23:26 -0300

    Configure root colors and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home';
 import { GlobalStorage } from './Contexts/GlobalContext';
+import Home from './Components/Home';
+import Details from './Components/Details/index';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route path="rest-countries-api-with-color-theme-switcher" element={<Home />} /* this route is only for use on GitHub Pages */ />
+          <Route path="/details" element={<Details />} />
         </Routes>
       </GlobalStorage>
     </BrowserRouter>
